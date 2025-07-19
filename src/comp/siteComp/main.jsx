2f758e5f66007e7617d97a28d9ca9255c1a7b7bd
Hardeep-singh <harseepsinghsardar@gmail.com>
2025-07-19 19:47:03 +0530

    Initial upload of all project files

--- src/comp/siteComp/main.jsx
+++ src/comp/siteComp/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Navbar from "./Navbar";
-import Footer from "./Footer";
-import Slidebar from "./Slidebar";
+import Footer from "./footer";
 import { Outlet } from "react-router-dom";
 import style from "./main.module.css";
 
